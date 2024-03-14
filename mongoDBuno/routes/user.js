62e846195b33429421dd5e82b63f314a88a66e61
David71-star <davidcortes024@gmail.com>
2024-03-14 18:31:39 +0100

    Create user.js

---
+++ mongoDBuno/routes/user.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+const UserModel = require("../modules/users");
+const { model } = require("mongoose");
+
+module.exports = router;
